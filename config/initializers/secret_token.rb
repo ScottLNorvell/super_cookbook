5b0a9210611ad6a5ab31eacecf0abd77491459af
Scott Norvell <scottlnorvell@gmail.com>
2013-09-17 15:32:58 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperCookbook::Application.config.secret_token = '96d0beb169f8d8a5ee0583c042ead052bfbe3a6f7a1566cae3fffe8a0d1fda396852fa5ec6aef20710b4c16ec210f4be51a74cf39ce075acffb9d38ea99ea102'
